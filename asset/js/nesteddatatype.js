b4a8c64df6bd3617c45d7e3177c623bf0482084f
Giacomo Nanni <gn.nanni@gmail.com>
2021-05-04 19:23:04 +0200

    fix updated class names

--- asset/js/nesteddatatype.js
+++ asset/js/nesteddatatype.js
@@ -1,21 +1,21 @@
 $(document).on('o:prepare-value', function (e, type, value, valueObj) {
     const thisValue = $(value);
-    const addBtn = thisValue.find('.add-property');
-    const container = thisValue.find('.properties');
+    const addBtn = thisValue.find('.nested-data-type_add_property');
+    const container = thisValue.find('.nested-data-type_properties');
 
     // I have to add a default Value to trigger the hydrate() function
-    const defaultValue = thisValue.find('.nested-data-type-value').attr({ 'value': 'value' });
+    const defaultValue = thisValue.find('.nested-data-type_value').attr({ 'value': 'value' });
 
     // Add item on click
     addBtn.on('click', function (e) {
         e.preventDefault();
-        const num = container.find('.repeat-property').length;
-        const clone = container.append(container.find('.repeat-property').last().clone());
+        const num = container.find('.nested-data-type_repeat_property').length;
+        const clone = container.append(container.find('.nested-data-type_repeat_property').last().clone());
 
-        const select = container.find('.repeat-property').last().find('select');
+        const select = container.find('.nested-data-type_repeat_property').last().find('select');
         select.attr({ 'data-value-key': `property-label-${num + 1}` });
 
-        const textarea = container.find('.repeat-property').last().find('textarea');
+        const textarea = container.find('.nested-data-type_repeat_property').last().find('textarea');
         textarea.attr({ 'data-value-key': `property-value-${num + 1}` });
         textarea.val('');
     });
@@ -28,17 +28,16 @@ $(document).on('o:prepare-value', function (e, type, value, valueObj) {
             const keys = Object.keys(properties);
             
             keys.forEach(function (element, i) {
-                const container = thisValue.find('.properties');
-                const select = container.find('.repeat-property').last().find('select');
-                const textarea = container.find('.repeat-property').last().find('textarea');
+                const container = thisValue.find('.nested-data-type_properties');
+                const select = container.find('.nested-data-type_repeat_property').last().find('select');
+                const textarea = container.find('.nested-data-type_repeat_property').last().find('textarea');
 
-                console.log(element, properties[element])
                 if(i == 0){
                     select.val(element);
                     textarea.val(properties[element]);
                 }
                 else{
-                    const clone = container.append(container.find('.repeat-property').last().clone());
+                    const clone = container.append(container.find('.nested-data-type_repeat_property').last().clone());
 
                     select.attr({ 'data-value-key': `property-label-${i + 1}` });
                     select.val(element);
