263f217f4259bcb2a9f389e4b6609675c4fe751c
Giacomo Nanni <gn.nanni@gmail.com>
2021-05-06 20:48:16 +0200

    fix arrow funct, input id

--- asset/js/nesteddatatype.js
+++ asset/js/nesteddatatype.js
@@ -1,10 +1,13 @@
-$(document).on('o:prepare-value', (e, type, value, valueObj) => {
+$(document).on('o:prepare-value', function (e, type, value, valueObj) {
     const thisValue = $(value);
     const container = thisValue.find('.nested-data-type_properties');
     const addBtn = thisValue.find('.nested-data-type_add_property');
     const rmvBtn = $('.nested-data-type_remove_property');
+
     // I have to add a default Value to trigger the hydrate() function
     const defaultValue = thisValue.find('.nested-data-type_value').val('value');
+    const defaultProeprty = thisValue.find('.nested-data-type_property').val('value');
+
     let select, textarea;
     const findItems = () => {
         select = container.find('.nested-data-type_repeat_property').last().find('.nested-data-type_property_dropdown');
@@ -13,18 +16,18 @@ $(document).on('o:prepare-value', (e, type, value, valueObj) => {
     const cloneItem = () => container.append(container.find('.nested-data-type_repeat_property').last().clone());
 
     // Add item on click
-    addBtn.on('click', (e) => {
+    addBtn.on('click', function (e) {
         e.preventDefault();
         const num = container.find('.nested-data-type_repeat_property').length;
-        findItems();
         cloneItem();
+        findItems();
         select.attr({ 'data-value-key': `property-label-${num + 1}` });
         select.val('');
         textarea.attr({ 'data-value-key': `property-value-${num + 1}` });
         textarea.val('');
     });
 
-    rmvBtn.on('click', (e) => {
+    rmvBtn.on('click', function (e) {
         e.preventDefault();
         $(this).parent().remove();
     });
