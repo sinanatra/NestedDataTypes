55d6506d201e4b2c847f83b32f52e98551c029d7
Giacomo Nanni <gn.nanni@gmail.com>
2021-07-21 10:09:01 +0200

    fix: ignore undefined values

--- asset/js/nesteddatatype.js
+++ asset/js/nesteddatatype.js
@@ -4,7 +4,6 @@ $(document).on('o:prepare-value', function (e, type, value, valueObj) {
     const thisValue = $(value);
     const container = thisValue.find('.nested-data-type_properties');
     const addBtn = thisValue.find('.nested-data-type_add_property');
-    const selectResource = $('.nested-data-type__resource_link');
 
     // Add a default Value to trigger the hydrate() function
     const defaultValue = thisValue.find('.nested-data-type_value').val('value');
@@ -72,112 +71,50 @@ $(document).on('o:prepare-value', function (e, type, value, valueObj) {
 
             keys.forEach((element, idx) => {
                 let item = properties[0][element];
-                let val = item[0]
-                if (typeof val === "object") {
-                    if (idx == 1) {
-                        findItems();
-                        select.val(element);
-                        if (val['@value']) textareaValue.val(val['@value']);
-                        if (val['label']) textareaValue.val(val['label']);
-                        if (val['@id']) textareaUri.val(val['@id']);
-                        if (val['@id'] && val['@id'].includes('/api/items/')) {
-
-                            container.find('.nested-data-type_repeat_property')
-                                .last()
-                                .find('.o-title.items')
-                                .remove();
-
-                            container.find('.nested-data-type_repeat_property')
-                                .last()
-                                .find('.input')
-                                .css('display', 'none');
-                            let link = `<span class="o-title items ml"><a href="${url}"> ${label}</a></span>`
-                            container.append(container.find('.nested-data-type_repeat_property').last().append(link));
-                        }
-                    }
-
-                    else if (idx > 1) {
-                        cloneItem();
-                        findItems();
-                        if (renderedLink) {
-                            renderedLink.remove();
-                            textareaValue.parent().parent().css('display', 'block');
-                            textareaUri.parent().parent().css('display', 'block')
-                        }
-                        textareaValue.attr({ 'data-value-key': `property-value-${idx}` }).val('');
-                        textareaUri.attr({ 'data-value-key': `property-uri-${idx}` }).val('');
-                        select.attr({ 'data-value-key': `property-label-${idx}` }).val(element);
-                        innerClass.attr({ 'data-value-key': `inner-class-${idx}` }).val('')
-                            .parent().css('display', 'none');
-                        innerProperty.attr({ 'data-value-key': `inner-property-${idx}` }).val('');
-
-                        if (val['@value']) { textareaValue.attr({ 'data-value-key': `property-value-${idx}` }).val(val['@value']) };
-                        if (val['label']) textareaValue.attr({ 'data-value-key': `property-value-${idx}` }).val(val['label']);
-                        if (val['@id']) textareaUri.attr({ 'data-value-key': `property-uri-${idx}` }).val(val['@id']);
-
-                        if (val['@id'] && val['@id'].includes('/api/items/')) {
-                            container.find('.nested-data-type_repeat_property')
-                                .last()
-                                .find('.o-title.items')
-                                .remove();
-
-                            container.find('.nested-data-type_repeat_property')
-                                .last()
-                                .find('.input')
-                                .css('display', 'none');
-
-                            let changeLinkView = val['@id'].replace('/api/items/', '/admin/item/');
-                            let link = `<span class="o-title items ml"><a href="${changeLinkView}"> ${val['label']}</a></span>`
-                            container.append(container.find('.nested-data-type_repeat_property').last().append(link));
-                        }
-                    }
-
-                    for (const [key, value] of Object.entries(val)) {
-                        if (key == '@type') {
-                            innerClass.val(value)
-                                .parent().css('display', 'block');
-                        }
+                for (i in item) {
+                    let val = item[i];
+                    if (typeof val === "object") {
                         if (idx == 1) {
-                            if (val[key]['@value']) {
-                                innerProperty.val(key);
-                                innerProperty.parent().css('display', 'block');
-                                textareaValue.val(val[key]['@value']);
-                            }
-                            if (val[key]['@id']) {
-                                innerProperty.val(key);
-                                textareaValue.val(val[key]['@id']);
-                            }
-                            if (val[key]['label']) textareaValue.val(val[key]['label']);
+                            findItems();
+                            select.val(element);
 
-                            if (val[key]['@id'] && val[key]['@id'].includes('/api/items/')) {
-                                container.find('.nested-data-type_repeat_property')
-                                    .last()
-                                    .find('.o-title.items')
-                                    .remove();
-
+                            if (val['@value'] && typeof val['@value'] !== "undefined") textareaValue.val(val['@value']);
+                            if (val['label'] && typeof val['label'] !== "undefined") textareaValue.val(val['label']);
+                            if (val['@id'] && typeof val['@id'] !== "undefined") textareaUri.val(val['@id']);
+                            if (val['@id'] && val['@id'].includes('/api/items/')) {
+                            
                                 container.find('.nested-data-type_repeat_property')
                                     .last()
                                     .find('.input')
                                     .css('display', 'none');
 
-                                let changeLinkView = val[key]['@id'].replace('/api/items/', '/admin/item/');
-                                let link = `<span class="o-title items ml"><a href="${changeLinkView}"> ${val[key]['label']}</a></span>`
+                                let changeLinkView = val['@id'].replace('/api/items/', '/admin/item/');
+                                let link = `<div class="o-title items ml"><a href="${changeLinkView}"> ${val['label']}</a></div>`;
+
                                 container.append(container.find('.nested-data-type_repeat_property').last().append(link));
                             }
                         }
+
                         else if (idx > 1) {
-                            innerClass.attr({ 'data-value-key': `inner-class-${idx}` });
-                            innerProperty.attr({ 'data-value-key': `inner-property-${idx}` }).val(key);
-
-                            if (val[key]['@value']) {
-                                textareaValue.attr({ 'data-value-key': `property-value-${idx}` }).val(val[key]['@value']);
+                            cloneItem();
+                            findItems();
+                            if (renderedLink) {
+                                renderedLink.remove();
+                                textareaValue.parent().parent().css('display', 'block');
+                                textareaUri.parent().parent().css('display', 'block')
                             }
-                            if (val[key]['label']) {
-                                textareaValue.attr({ 'data-value-key': `property-value-${idx}` }).val(val[key]['label']);
-                            }
-                            if (val[key]['@id']) textareaUri.attr({ 'data-value-key': `property-uri-${idx}` }).val(val[key]['@id']);
+                            textareaValue.attr({ 'data-value-key': `property-value-${idx}` }).val('');
+                            textareaUri.attr({ 'data-value-key': `property-uri-${idx}` }).val('');
+                            select.attr({ 'data-value-key': `property-label-${idx}` }).val(element);
+                            innerClass.attr({ 'data-value-key': `inner-class-${idx}` }).val('')
+                                .parent().css('display', 'none');
+                            innerProperty.attr({ 'data-value-key': `inner-property-${idx}` }).val('');
 
-                            if (val[key]['@id'] && val[key]['@id'].includes('/api/items/')) {
+                            if (val['@value'] && typeof val['@value'] !== "undefined") { textareaValue.attr({ 'data-value-key': `property-value-${idx}` }).val(val['@value']) };
+                            if (val['label'] && typeof val['label'] !== "undefined") textareaValue.attr({ 'data-value-key': `property-value-${idx}` }).val(val['label']);
+                            if (val['@id'] && typeof val['@id'] !== "undefined") textareaUri.attr({ 'data-value-key': `property-uri-${idx}` }).val(val['@id']);
+
+                            if (val['@id'] && val['@id'].includes('/api/items/')) {
                                 container.find('.nested-data-type_repeat_property')
                                     .last()
                                     .find('.o-title.items')
@@ -188,9 +125,72 @@ $(document).on('o:prepare-value', function (e, type, value, valueObj) {
                                     .find('.input')
                                     .css('display', 'none');
 
-                                let changeLinkView = val[key]['@id'].replace('/api/items/', '/admin/item/');
-                                let link = `<span class="o-title items ml"><a href="${changeLinkView}"> ${val[key]['label']}</a></span>`
+                                let changeLinkView = val['@id'].replace('/api/items/', '/admin/item/');
+                                let link = `<div class="o-title items ml"><a href="${changeLinkView}"> ${val['label']}</a></div>`
                                 container.append(container.find('.nested-data-type_repeat_property').last().append(link));
+                            }
+                        }
+
+                        for (const [key, value] of Object.entries(val)) {
+                            if (key == '@type') {
+                                innerClass.val(value)
+                                    .parent().css('display', 'block');
+                            }
+                            if (idx == 1) {
+                                if (val[key]['@value'] && typeof val[key]['@value'] !== "undefined") {
+                                    innerProperty.val(key);
+                                    innerProperty.parent().css('display', 'block');
+                                    textareaValue.val(val[key]['@value']);
+                                }
+                                if (val[key]['@id'] && typeof val[key]['@id'] !== "undefined") {
+                                    innerProperty.val(key);
+                                    textareaValue.val(val[key]['@id']);
+                                }
+                                if (val[key]['label'] && typeof val[key]['label'] !== "undefined") textareaValue.val(val[key]['label']);
+
+                                if (val[key]['@id'] && val[key]['@id'].includes('/api/items/')) {
+                                    container.find('.nested-data-type_repeat_property')
+                                        .last()
+                                        .find('.o-title.items')
+                                        .remove();
+
+                                    container.find('.nested-data-type_repeat_property')
+                                        .last()
+                                        .find('.input')
+                                        .css('display', 'none');
+
+                                    let changeLinkView = val[key]['@id'].replace('/api/items/', '/admin/item/');
+                                    let link = `<div class="o-title items ml"><a href="${changeLinkView}"> ${val[key]['label']}</a></div>`
+                                    container.append(container.find('.nested-data-type_repeat_property').last().append(link));
+                                }
+                            }
+                            else if (idx > 1) {
+                                innerClass.attr({ 'data-value-key': `inner-class-${idx}` });
+                                innerProperty.attr({ 'data-value-key': `inner-property-${idx}` }).val(key);
+
+                                if (val[key]['@value'] && typeof val[key]['@value'] !== "undefined") {
+                                    textareaValue.attr({ 'data-value-key': `property-value-${idx}` }).val(val[key]['@value']);
+                                }
+                                if (val[key]['label'] && typeof val[key]['label'] !== "undefined") {
+                                    textareaValue.attr({ 'data-value-key': `property-value-${idx}` }).val(val[key]['label']);
+                                }
+                                if (val[key]['@id'] && typeof val[key]['@id'] !== "undefined") textareaUri.attr({ 'data-value-key': `property-uri-${idx}` }).val(val[key]['@id']);
+
+                                if (val[key]['@id'] && val[key]['@id'].includes('/api/items/')) {
+                                    container.find('.nested-data-type_repeat_property')
+                                        .last()
+                                        .find('.o-title.items')
+                                        .remove();
+
+                                    container.find('.nested-data-type_repeat_property')
+                                        .last()
+                                        .find('.input')
+                                        .css('display', 'none');
+
+                                    let changeLinkView = val[key]['@id'].replace('/api/items/', '/admin/item/');
+                                    let link = `<div class="o-title items ml"><a href="${changeLinkView}"> ${val[key]['label']}</a></div>`
+                                    container.append(container.find('.nested-data-type_repeat_property').last().append(link));
+                                }
                             }
                         }
                     }
@@ -213,7 +213,7 @@ $(document).on('o:prepare-value', function (e, type, value, valueObj) {
 
             cloneItem();
             findItems();
-            
+
             if (renderedLink) {
                 renderedLink.remove();
                 textareaValue.parent().parent().css('display', 'block');
@@ -242,9 +242,70 @@ $(document).on('o:prepare-value', function (e, type, value, valueObj) {
                 .remove();
 
 
-            let link = `<span class="o-title items ml"><a href="${url}"> ${label}</a></span>`
+            let link = `<div class="o-title items ml"><a href="${url}"> ${label}</a></div>`
 
             container.append(container.find('.nested-data-type_repeat_property').last().append(link));
+
+        };
+    });
+
+    $(document).on('click', '.nested-data-type__resource_multiple', function (e) {
+        e.preventDefault();
+        let checkbox = $('.items  input:checked').parent().parent();
+
+        cloneItem();
+        findItems();
+
+        const num = container.find('.nested-data-type_repeat_property').length;
+
+        if (thisValue.is('.selecting-resource')) {
+
+            let id = [], label = [], url = [];
+
+            $(checkbox).each(element => {
+                if ($(checkbox[element]).hasClass('resource')) {
+                    const resource = JSON.parse($(checkbox[element]).attr('data-resource-values'));
+                    id.push(resource['@id']);
+                    label.push(resource['display_title']);
+                    url.push(resource['url']);
+                }
+            });
+
+            if (renderedLink) {
+                renderedLink.remove();
+                textareaValue.parent().parent().css('display', 'block');
+                textareaUri.parent().parent().css('display', 'block')
+            }
+
+            select.attr({ 'data-value-key': `property-label-${num + 1}` })
+                .val('');
+            textareaValue.attr({ 'data-value-key': `property-value-${num + 1}` })
+                .val(label)
+            textareaUri.attr({ 'data-value-key': `property-uri-${num + 1}` })
+                .val(id)
+            innerClass.attr({ 'data-value-key': `inner-class-${num + 1}` })
+                .val('');
+            innerProperty.attr({ 'data-value-key': `inner-property-${num + 1}` })
+                .val('');
+
+            container.find('.nested-data-type_repeat_property')
+                .last()
+                .find('.input')
+                .css('display', 'none');
+
+            container.find('.nested-data-type_repeat_property')
+                .last()
+                .find('.o-title.items')
+                .remove();
+
+            for (let index = 0; index < id.length; index++) {
+                const singleId = id[index];
+                const singleUrl = url[index];
+                const singleLabel = label[index];
+
+                let link = `<div class="o-title items ml"><a href="${singleUrl}"> ${singleLabel}</a></div>`
+                container.append(container.find('.nested-data-type_repeat_property').last().append(link));
+            }
 
         };
     });
